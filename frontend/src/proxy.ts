7d9a01a78ec3b0731b68fc27ef89afb3b482a50a
hakenshi <fkdias025@gmail.com>
2025-11-28 13:10:19 -0300

    feat(frontend): add proxy configuration for API requests

---
+++ frontend/src/proxy.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+    // const token = request.cookies.get("authToken")
+
+    // if (!token) {
+    //     return NextResponse.redirect(new URL("/login", request.url))
+    // }
+    // return NextResponse.redirect(new URL("/", request.url))
+}
