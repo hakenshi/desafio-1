c63a2dc46a339d9168a00b61c33740e54d24e1c0
hakenshi <fkdias025@gmail.com>
2025-11-30 01:44:02 -0300

    refactor(frontend): update proxy configuration

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-    // const token = request.cookies.get("authToken")
+    const token = request.cookies.get("authToken")
 
-    // if (!token) {
-    //     return NextResponse.redirect(new URL("/login", request.url))
-    // }
-    // return NextResponse.redirect(new URL("/", request.url))
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
+    ]
 }
